65f2e3d743ce5b235fbb64a1ceb15c9387a7f1eb
makotot <makoto.ttn@gmail.com>
2020-04-03 01:32:11 +0900

    remove

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Box } from './Box';
-import { Flex } from './Flex';
-import { Grid } from './Grid';
-import { Position } from './Position';
-import { Text } from './Text';
-import * as types from './types';
-export { Box, Flex, Grid, Position, Text, types, };
