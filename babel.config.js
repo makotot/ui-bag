6ce865ff3885cb9f4eaef19fcd26205440d9ef30
makotot <makoto.ttn@gmail.com>
2020-04-04 12:01:39 +0900

    fix build

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [
     ['@babel/env', {
       modules: process.env.BABEL_ENV === 'cjs' ? 'commonjs' : false,
+      loose: true,
     }],
     '@babel/preset-react',
     '@babel/preset-typescript',
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import { Box } from './Box'
-import { Flex } from './Flex'
-import { Grid } from './Grid'
-import { Position } from './Position'
-import { Text } from './Text'
+export { Box } from './Box'
+export { Flex } from './Flex'
+export { Grid } from './Grid'
+export { Position } from './Position'
+export { Text } from './Text'
 import * as types from './types'
 
 export {
-  Box,
-  Flex,
-  Grid,
-  Position,
-  Text,
   types,
 }
